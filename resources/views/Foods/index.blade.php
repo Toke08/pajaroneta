8d03f8415a2a09da57c28bbcc458b391763d30d1
Toke08 <122867024+Toke08@users.noreply.github.com>
2024-01-14 20:45:09 +0100

    hola

--- resources/views/Foods/index.blade.php
+++ resources/views/Foods/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layout.masterpage')
+
 @section('titulo')
     Galeria de comidas
 @endsection
+
 @section('estilos')
 <style>
     body{
@@ -9,14 +11,10 @@ background-color: rgb(167, 159, 45)
 }
 </style>
 @endsection
+
 @section('contenido')
+
     <h1>Galeria de comidas</h1>
-    {{-- <ul>
-        @foreach ($users as $user)
-            <li> <strong>ID</strong>: {{ $user->id }}, <strong>Nombre: </strong>: <a
-                    href='usuario/{{ $user->id }}'>{{ $user->name }}</a></li>
-        @endforeach
-    </ul> --}}
 
         @foreach ($foods as $food)
         <div class="food-container">
@@ -24,4 +22,5 @@ background-color: rgb(167, 159, 45)
             <h2>{{$food->name}}</h2>
         </div>
         @endforeach
+
 @endsection

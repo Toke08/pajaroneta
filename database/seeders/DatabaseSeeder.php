48a40b4ea8433afe4da832d714aac8818eb88e31
Toke08 <122867024+Toke08@users.noreply.github.com>
2024-01-15 21:04:27 +0100

    food index (galeria de comidas)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,62 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Food::factory()->create([
+        \App\Models\Food::create([
             "name"=> "La maleducada",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "La Cubana",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "La Española",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "No Gluten",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "Cuasó",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "la funfuñosa",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "4kilos",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "La maleducada XXL",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "La perruna",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+        ]);
+        \App\Models\Food::create([
+            "name"=> "pajaro loco",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
--- resources/views/Foods/index.blade.php
+++ resources/views/Foods/index.blade.php
@@ -4,23 +4,52 @@
     Galeria de comidas
 @endsection
 
-@section('estilos')
+{{-- @section('estilos')
 <style>
     body{
 background-color: rgb(167, 159, 45)
 }
+.foods-container{
+display: grid;
+}
+.food-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
 </style>
-@endsection
+@endsection --}}
 
 @section('contenido')
+<style>
+body{
+    background-color: rgb(230, 230, 230)
+    }
+    .foods-container{
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap:1rem;
 
+    }
+    .food-container{
+
+
+
+    }
+    .food-container img{
+        min-width: 100%;
+        border-radius: 5%;
+
+    }
+</style>
     <h1>Galeria de comidas</h1>
-
+        <div class="foods-container">
         @foreach ($foods as $food)
         <div class="food-container">
-            <img src="storage/img/{{$food->img}}">
+            <img src="{{ asset('img/'.$food->img) }}" style="width: 20%; aspect-ratio:1;">
             <h2>{{$food->name}}</h2>
         </div>
         @endforeach
+    </div>
 
 @endsection

7eff2910bc65f2240e526cd39352f917f3d670c2
roddigoz6 <131356598+roddigoz6@users.noreply.github.com>
2024-01-24 13:14:09 +0100

    Commit 24/01, 13:13

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
+            $table->date('birthday');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('type');
+            $table->Integer('role_id');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 

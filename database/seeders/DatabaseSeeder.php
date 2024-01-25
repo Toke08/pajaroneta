397c306e8d8aceff5ee5af415193f332bf9552a7
Toke08 <122867024+Toke08@users.noreply.github.com>
2024-01-25 09:41:22 +0100

    correccion seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,71 +21,103 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+
+        \App\Models\Category::create([
+            "id"   => 1,
+            "name" => "Hamburguesas",
+            "img" => "hamburguesas.jpg",
+        ]);
+
+        \App\Models\Category::create([
+            "id"   => 2,
+            "name"=> "Hot-Dogs",
+            "img" => "hotdogs.jpg",
+        ]);
+
         \App\Models\Food::create([
-            "name"=> "La maleducada",
+            "name"=> "La maleducada pipi",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
+        ]);
+
+        \App\Models\Food::create([
+            "name"=> "La maleducada pi",
+            "price"   => 6,
+            "img" => "default.jpg",
+            "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "La Cubana",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "La Española",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "No Gluten",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "Cuasó",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "la funfuñosa",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "4kilos",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "La maleducada XXL",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "La perruna",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "pajaro loco",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
         \App\Models\Food::create([
             "name"=> "pajaro loco2",
             "price"   => 6,
             "img" => "default.jpg",
             "description" => "la mejor hamburguesa del mundo ñam ñam",
+            "category_id" => 1,
         ]);
     }
 }

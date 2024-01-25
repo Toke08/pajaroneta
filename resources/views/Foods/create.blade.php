35cd52ab47b15958276edce1e410640681343f7f
Toke08 <122867024+Toke08@users.noreply.github.com>
2024-01-25 12:32:36 +0100

    funcion crear comida hecho

--- resources/views/Foods/create.blade.php
+++ resources/views/Foods/create.blade.php
@@ -18,10 +18,18 @@ Crear comida nueva
         @csrf
 
         <label for="name">Nombre:</label>
+        <br>
         <input type="text" id="name" name="name" required>
         <br>
+        <label for="description">Descripcion:</label>
+        <br>
+        <textarea id="description" name="description" rows="4" cols="50" required></textarea>
+        <br>
         <label for="price">Precio:</label>
+        <br>
         <input type="text" id="price" name="price" required>
+        <br>
+        <label for="categories">categoria:</label>
         <br>
         <select name="categories" id="">
             @foreach($categories as $category)

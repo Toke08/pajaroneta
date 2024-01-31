0f51a036bd57eb75b31680444dbc92a90db070c4
roddigoz6 <131356598+roddigoz6@users.noreply.github.com>
2024-01-31 10:51:49 +0100

    Blog y comentarios client
    Tengo miedo :(

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Food;
 use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Category;
 
 class ClientController extends Controller
 {
@@ -18,6 +20,18 @@ class ClientController extends Controller
     public function blog(){
         $posts = Post::all();
         return view("client.blog", ['posts'=> $posts]);
+    }
+
+    public function blog_show($id){
+        $post = Post::with('tag')->findOrFail($id);
+        $comments = Comment::where('post_id', $post->id)->get();
+        return view('blog.show', compact('post', 'comments'));
+    }
+
+    public function category()
+    {
+        $categories = Category::all();
+        return view("client.categorias", ['categories'=> $categories]);
     }
 
 }

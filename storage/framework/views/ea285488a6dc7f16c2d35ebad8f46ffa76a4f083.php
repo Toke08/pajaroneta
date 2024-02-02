107219d51af35a3853c5b9e3b7f59cd037489a8f
roddigoz6 <131356598+roddigoz6@users.noreply.github.com>
2024-02-02 13:56:25 +0100

    Nos fuimos

--- storage/framework/views/ea285488a6dc7f16c2d35ebad8f46ffa76a4f083.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('contenido'); ?>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.masterpage', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Rod\Desktop\Desk\DAW\UniServerZ\www\pajaroneta\resources\views/index.blade.php ENDPATH**/ ?>

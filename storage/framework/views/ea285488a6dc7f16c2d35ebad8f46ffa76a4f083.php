8fddf7e74872a9a67b0de4b6a802145870c0a061
roddigoz6 <131356598+roddigoz6@users.noreply.github.com>
2024-02-03 21:44:33 +0100

    Multi part

---
+++ storage/framework/views/ea285488a6dc7f16c2d35ebad8f46ffa76a4f083.php
@@ -0,0 +1,6 @@
+
+<?php $__env->startSection('contenido'); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.masterpage', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Rod\Desktop\Desk\DAW\UniServerZ\www\pajaroneta\resources\views/index.blade.php ENDPATH**/ ?>

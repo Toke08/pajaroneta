b3ae50a7ea5ea4e9e9c3e3d578fc1f08c71f6c96
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-05 13:21:33 +0100

    nav con estilos 1

--- storage/framework/views/5a979759fc69d94b4c80410a66a9169eb71ea0cb.php
+++ storage/framework/views/5a979759fc69d94b4c80410a66a9169eb71ea0cb.php
@@ -1,10 +1,15 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<div class="custom-shape-divider-top-1707134803">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+</div>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top ">
     <a class="navbar-brand" href="<?php echo e(route('home')); ?>">Pajaroneta</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo e(route('galeria_comidas')); ?>"><?php echo app('translator')->get('Food Gallery'); ?></a>
             </li>
@@ -22,7 +27,7 @@
     <li class="nav-item">
         <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo app('translator')->get('Register'); ?></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item1">
         <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo app('translator')->get('Login'); ?></a>
     </li>
 <?php else: ?>

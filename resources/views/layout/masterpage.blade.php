3134d83e4b6f38c7c18a7b6615a46cfcb07ee463
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-08 13:40:02 +0100

    estilos con poco estilo

--- resources/views/layout/masterpage.blade.php
+++ resources/views/layout/masterpage.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pajaroneta @yield('titulo')</title>
 
-    <!-- CSS de Bootstrap -->
+    <!-- CSS de Bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"> -->
 
     <!-- Estilos personalizados -->
     <link rel="preconnect" href="https://fonts.googleapis.com">

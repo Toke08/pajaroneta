8feb56ff523ca871fce11f8b9ee681b0b109c81c
Toke08 <122867024+Toke08@users.noreply.github.com>
2024-02-08 10:29:30 +0100

    empezamos con plantilla dashboard intento 2

--- storage/framework/views/323fa81d6810119de92438bc5b7320726d76860a.php
+++ storage/framework/views/323fa81d6810119de92438bc5b7320726d76860a.php
@@ -1,4 +1,3 @@
-
 <?php $__env->startSection('titulo'); ?>
 <?php $__env->stopSection(); ?>
 
--- storage/framework/views/8853b3298f8f88332a790c013cc2b19235ad9534.php
+++ storage/framework/views/8853b3298f8f88332a790c013cc2b19235ad9534.php
@@ -6,6 +6,7 @@
     <title>Pajaroneta <?php echo $__env->yieldContent('titulo'); ?></title>
 
     <!-- CSS de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo e(asset('css/bootstrap.min.css')); ?>" rel="stylesheet">
 
@@ -14,6 +15,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="<?php echo e(asset('css/navbar-top-fixed.css')); ?>" rel="stylesheet">
     <link href="<?php echo e(asset('css/general.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('css/footer.css')); ?>" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -63,8 +65,9 @@
     </footer>
 
     <!-- JS de Bootstrap (popper.js y Bootstrap JS) -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 <?php /**PATH D:\UniServerZ\www\pajaroneta\resources\views/layout/masterpage.blade.php ENDPATH**/ ?>

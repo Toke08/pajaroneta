2b9475b55ecd882fe71ac32d5c076f04ac2c108a
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-09 13:16:14 +0100

    cambioss

--- storage/framework/views/fa736c7c3b599ba4201e02244149b447b3e8a732.php
+++ storage/framework/views/fa736c7c3b599ba4201e02244149b447b3e8a732.php
@@ -2,6 +2,7 @@
 
 
 <?php $__env->startSection('titulo'); ?>
+Calendario
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('estilos'); ?>
@@ -16,16 +17,30 @@
 iframe{
     width: 100%;
 }
+
+/* para el modal */
+
+.modal-body{
+    background-image: url("<?php echo e(asset('img/trucks.png')); ?>");
+}
+
+#btn_save{
+    background-color:#E5A200;
+    border: none;
+    border-radius: 1.5em;
+    padding: 2%;
+    width: 7em;
+    
+}
+#btn_save:hover{
+    background-color:#CA8F00;
+}
+
+
 </style>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('contenido'); ?>
-
-<h1>Hoy nos encontramos en...</h1>
-
-<div id="mapa">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.5165004229702!2d-2.941988723335174!3d43.26155407767247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502842c84087%3A0x539b319a98f8cfbe!2sC.%20del%20Lic.%20Poza%2C%2031%2C%20Abando%2C%2048011%20Bilbao%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1707304758249!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
 
 <div id="caja_info"></div>
 

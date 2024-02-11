9f8299761e33824cd8e302db71b0e802e2dee6cf
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-11 15:43:31 +0100

    comiteo

--- storage/framework/views/84e5bfce60e5bf68d78d7b77b839d9575e2c3cfc.php
+++ storage/framework/views/84e5bfce60e5bf68d78d7b77b839d9575e2c3cfc.php
@@ -4,10 +4,16 @@
     </svg>
 </div>
 <nav class="navbar navbar-expand-md navbar-dark fixed-top">
-    <a class="navbar-brand" href="<?php echo e(route('home')); ?>">Pajaroneta</a>
+    <a class="navbar-brand" href="<?php echo e(route('home')); ?>">
+        <img id="logo" src="<?php echo e(asset('img/landing_page/pajaro-01.png')); ?>"alt="">
+    </a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span> <!-- Nueva barra -->
+        <span class="navbar-toggler-icon"></span> <!-- Nueva barra -->
     </button>
+
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
@@ -24,32 +30,32 @@
             </li>
 
             <?php if(auth()->guard()->guest()): ?>
-    
-    <li class="nav-item">
-        <a id="login-btn" class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo app('translator')->get('Login'); ?></a>
-    </li>
-<?php else: ?>
-    <?php if(auth()->user()->isAdmin()): ?>
-        <!-- Si es un administrador, redirigir al dashboard -->
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo e(route('adminHome')); ?>"><?php echo app('translator')->get('Hello,'); ?> <?php echo e(auth()->user()->name); ?></a>
-        </li>
+            
+            <li class="nav-item">
+                <a id="login-btn" class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo app('translator')->get('Login'); ?></a>
+            </li>
+        <?php else: ?>
+            <?php if(auth()->user()->isAdmin()): ?>
+                <!-- Si es un administrador, redirigir al dashboard -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('adminHome')); ?>"><?php echo app('translator')->get('Hello,'); ?> <?php echo e(auth()->user()->name); ?></a>
+                </li>
 
-    <?php else: ?>
-        <!-- Si no es un administrador, mostrar el nombre del usuario -->
-        <li class="nav-item">
-            <a class="nav-link text-white" href="<?php echo e(route('user_show', ['name' => auth()->user()->name])); ?>"><?php echo app('translator')->get('Hello,'); ?> <?php echo e(auth()->user()->name); ?></a>
-        </li>
-    <?php endif; ?>
+            <?php else: ?>
+                <!-- Si no es un administrador, mostrar el nombre del usuario -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('user_show', ['name' => auth()->user()->name])); ?>"><?php echo app('translator')->get('Hello,'); ?> <?php echo e(auth()->user()->name); ?></a>
+                </li>
+            <?php endif; ?>
 
-    <!-- Común para usuarios y administradores -->
-    <li class="nav-item">
-        <a class="nav-link" href="#" onclick="event.preventDefault();document.getElementById('logout').submit();"><?php echo app('translator')->get('Logout'); ?></a>
-        <form id="logout" action="<?php echo e(route('logout')); ?>" method="POST">
-            <?php echo csrf_field(); ?>
-        </form>
-    </li>
-<?php endif; ?>
+            <!-- Común para usuarios y administradores -->
+            <li class="nav-item">
+                <a class="nav-link" href="#" onclick="event.preventDefault();document.getElementById('logout').submit();"><?php echo app('translator')->get('Logout'); ?></a>
+                <form id="logout" action="<?php echo e(route('logout')); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+                </form>
+            </li>
+        <?php endif; ?>
 
         </ul>
         <span class="text-white">
@@ -57,6 +63,7 @@
             <a href="<?php echo e(route('setLanguage','eu')); ?>">EU</a>
             <a href="<?php echo e(route('setLanguage', 'en')); ?>">EN</a>
         </span>
+
     </div>
 </nav>
 <?php /**PATH D:\UniServerZ\www\pajaroneta\resources\views/layout/nav.blade.php ENDPATH**/ ?>

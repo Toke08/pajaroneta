fde2fcc4c82f16dbbaa14e025e3efe225a5d62ea
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-13 00:30:40 +0100

    habemus index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,6 @@
 .landing_page {
     /* display:flex;
     flex-direction:row;*/
-    width: 2000px
     place-items:center;
     justify-content:center;
     display: grid;
@@ -61,6 +60,105 @@
     width:35em;
     height: 25em;
 }
+/* estilo mapa */
+
+#mapilla iframe{
+    width: 100%;
+}
+
+/* estilos restaurantes */
+.restaurantes{
+    width: 500px;
+    padding: 10px;
+    max-width: 500px;
+}
+.restaurantes img{
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#restaurant, .publis, #comidas, #mapilla{
+    margin-top: 10%;
+}
+.restaurants a{
+    color: #000000;
+    text-decoration: none;
+}
+#div-restaurantes{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px,1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+/* estilos post */
+#posts{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px,1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+
+#posts img{
+    max-width: 90%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#posts img:hover{
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    transition: box-shadow 0.3s ease-in-out;
+}
+#posts button{
+    background-color: #E5A200;
+    color: #ffffff;
+    border: none;
+    border-radius: 1.5em;
+    width: 30%;
+    padding: 2%;
+    transition: 0.3s ease-in-out;
+}
+#posts button:hover{
+    background-color: #CA8F00;
+    transition: box-shadow 0.3s ease-in-out;
+}
+#posts button a{
+    text-decoration: none;
+    color: #ffffff;
+}
+#posts button:active{
+    background-color: #CA8F00; /* Usa el mismo color que en :hover */
+    border: none;
+    border-radius: 1.5em;
+    box-shadow: none;
+}
+
+/* estilos comida */
+#comidita{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+
+#comidita img{
+    max-width: 90%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#comidita img:hover{
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    transition: box-shadow 0.3s ease-in-out;
+}
+
+
 </style>
 
 @endsection
@@ -91,31 +189,65 @@
         <img src="{{ asset('img/landing_page/burger_der.png') }}" alt="">
     </div>
     <div id="mapilla">
-
+        <div>
+            <h2>Encuentrános</h2>
+            <img src="" alt="">
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.5165004228597!2d-2.9419887246831182!3d43.26155407767463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502842c84087%3A0x539b319a98f8cfbe!2sC.%20del%20Lic.%20Poza%2C%2031%2C%20Abando%2C%2048011%20Bilbao%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1707666740981!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
     <div id="comidas">
+        <h2>¡Nuestra menú!</h2>
+        <p>Ven a comer con nosotros</p>
+        <div id="comidita">
+            @foreach ($foods->take(3) as $food)
+                <div class="com">
+                    <a href="galeria-comidas/{{ $food->name }}"><img src="{{asset('img/foods')}}/{{ $food->img }}"><img></a>
+                    <br>
+                    <h3>{{ $food->name }}</h3>
+                    <p>{{$food->description}}</p>
+                </div>
+            @endforeach
+        </div>
 
     </div>
 
-
-    <div id="blogges">
-
+    <div class="publis">
+        <h2>Opiniones y más</h2>
+        <p>Te invitamos a leer nuestro blog :)</p>
+        <div id="posts">
+            @foreach ($posts->take(2) as $post)
+                <div class="post">
+                    <a href="blog/{{ $post->id }}"><img src="{{asset('img/posts')}}/{{ $post->img }}"><img></a>
+                    <br>
+                    <h3>{{ $post->title }}</h3>
+                    <button> <a href="blog/{{ $post->id }}">Leer más</a></button>
+                </div>
+            @endforeach
+        </div>
     </div>
 
 
 
+    <div id="restaurant"></div>
+        <h2>Por si estamos lejos de ti...</h2>
+        <p>¡Encuentra restaurantes con opciones saludables cerca de ti!</p>
+        <div id="div-restaurantes">
             @foreach ($restaurants as $restaurant)
-            <div class="restaurants">
-                <h3><a href="{{ $restaurant->url_sitio }}" target="blank">{{ $restaurant->name }}</a></h3>
+            <div class="restaurantes">
+                <a href="{{ $restaurant->url_sitio }}" target="blank">
+                    <h3>{{ $restaurant->name }}</h3>
 
-                <p>{{ $restaurant->description }}</p>
+                    <p>{{ $restaurant->description }}</p>
 
-                <br>
-                <img src="{{ asset('img/restaurants') . '/' . $restaurant->img }}" alt="{{ $restaurant->name }}"><br>
+                    <br>
+                    <img src="{{ asset('img/restaurants') . '/' . $restaurant->img }}" alt="{{ $restaurant->name }}"><br>
+                </a>
                 <a href="{{ $restaurant->url_maps }}" target="blank">Encuéntralos aquí</a>
             </div>
             @endforeach
+        </div>
+    </div>
 
 
 
--- storage/framework/views/e8738279ac7584413ba69fcbce689ec00bef76a7.php
+++ storage/framework/views/e8738279ac7584413ba69fcbce689ec00bef76a7.php
@@ -4,7 +4,6 @@
 .landing_page {
     /* display:flex;
     flex-direction:row;*/
-    width: 2000px
     place-items:center;
     justify-content:center;
     display: grid;
@@ -59,6 +58,105 @@
     width:35em;
     height: 25em;
 }
+/* estilo mapa */
+
+#mapilla iframe{
+    width: 100%;
+}
+
+/* estilos restaurantes */
+.restaurantes{
+    width: 500px;
+    padding: 10px;
+    max-width: 500px;
+}
+.restaurantes img{
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#restaurant, .publis, #comidas, #mapilla{
+    margin-top: 10%;
+}
+.restaurants a{
+    color: #000000;
+    text-decoration: none;
+}
+#div-restaurantes{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px,1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+/* estilos post */
+#posts{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px,1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+
+#posts img{
+    max-width: 90%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#posts img:hover{
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    transition: box-shadow 0.3s ease-in-out;
+}
+#posts button{
+    background-color: #E5A200;
+    color: #ffffff;
+    border: none;
+    border-radius: 1.5em;
+    width: 30%;
+    padding: 2%;
+    transition: 0.3s ease-in-out;
+}
+#posts button:hover{
+    background-color: #CA8F00;
+    transition: box-shadow 0.3s ease-in-out;
+}
+#posts button a{
+    text-decoration: none;
+    color: #ffffff;
+}
+#posts button:active{
+    background-color: #CA8F00; /* Usa el mismo color que en :hover */
+    border: none;
+    border-radius: 1.5em;
+    box-shadow: none;
+}
+
+/* estilos comida */
+#comidita{
+    display: grid;
+    grid-gap:0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-auto-rows: auto;
+    grid-auto-flow: dense;
+}
+
+#comidita img{
+    max-width: 90%;
+    height: auto;
+    margin-bottom: 10px;
+    object-fit: cover;
+    border-radius: 10px;
+}
+#comidita img:hover{
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    transition: box-shadow 0.3s ease-in-out;
+}
+
+
 </style>
 
 <?php $__env->stopSection(); ?>
@@ -89,31 +187,65 @@
         <img src="<?php echo e(asset('img/landing_page/burger_der.png')); ?>" alt="">
     </div>
     <div id="mapilla">
-
+        <div>
+            <h2>Encuentrános</h2>
+            <img src="" alt="">
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.5165004228597!2d-2.9419887246831182!3d43.26155407767463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502842c84087%3A0x539b319a98f8cfbe!2sC.%20del%20Lic.%20Poza%2C%2031%2C%20Abando%2C%2048011%20Bilbao%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1707666740981!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
     <div id="comidas">
+        <h2>¡Nuestra menú!</h2>
+        <p>Ven a comer con nosotros</p>
+        <div id="comidita">
+            <?php $__currentLoopData = $foods->take(3); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $food): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <div class="com">
+                    <a href="galeria-comidas/<?php echo e($food->name); ?>"><img src="<?php echo e(asset('img/foods')); ?>/<?php echo e($food->img); ?>"><img></a>
+                    <br>
+                    <h3><?php echo e($food->name); ?></h3>
+                    <p><?php echo e($food->description); ?></p>
+                </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
 
     </div>
 
-
-    <div id="blogges">
-
+    <div class="publis">
+        <h2>Opiniones y más</h2>
+        <p>Te invitamos a leer nuestro blog :)</p>
+        <div id="posts">
+            <?php $__currentLoopData = $posts->take(2); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <div class="post">
+                    <a href="blog/<?php echo e($post->id); ?>"><img src="<?php echo e(asset('img/posts')); ?>/<?php echo e($post->img); ?>"><img></a>
+                    <br>
+                    <h3><?php echo e($post->title); ?></h3>
+                    <button> <a href="blog/<?php echo e($post->id); ?>">Leer más</a></button>
+                </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
     </div>
 
 
 
+    <div id="restaurant"></div>
+        <h2>Por si estamos lejos de ti...</h2>
+        <p>¡Encuentra restaurantes con opciones saludables cerca de ti!</p>
+        <div id="div-restaurantes">
             <?php $__currentLoopData = $restaurants; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $restaurant): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <div class="restaurants">
-                <h3><a href="<?php echo e($restaurant->url_sitio); ?>" target="blank"><?php echo e($restaurant->name); ?></a></h3>
+            <div class="restaurantes">
+                <a href="<?php echo e($restaurant->url_sitio); ?>" target="blank">
+                    <h3><?php echo e($restaurant->name); ?></h3>
 
-                <p><?php echo e($restaurant->description); ?></p>
+                    <p><?php echo e($restaurant->description); ?></p>
 
-                <br>
-                <img src="<?php echo e(asset('img/restaurants') . '/' . $restaurant->img); ?>" alt="<?php echo e($restaurant->name); ?>"><br>
+                    <br>
+                    <img src="<?php echo e(asset('img/restaurants') . '/' . $restaurant->img); ?>" alt="<?php echo e($restaurant->name); ?>"><br>
+                </a>
                 <a href="<?php echo e($restaurant->url_maps); ?>" target="blank">Encuéntralos aquí</a>
             </div>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+    </div>
 
 
 

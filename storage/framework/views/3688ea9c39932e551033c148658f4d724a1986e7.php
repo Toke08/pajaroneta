994d616afecd08d1026937cce8ea5bf8a3be3e4a
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-13 22:48:23 +0100

    estilos dash crear evento

---
+++ storage/framework/views/3688ea9c39932e551033c148658f4d724a1986e7.php
@@ -0,0 +1,57 @@
+
+
+<?php $__env->startSection('titulo'); ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('estilos'); ?>
+<style>
+
+</style>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('contenido'); ?>
+<h1>Tus ubicaciones</h1>
+<p>Estas son tus ubicaciones que usarás para asignar a tus eventos</p>
+<a href="<?php echo e(route('ubicaciones.create')); ?>" class="btn btn-primary">Crear nueva ubicación</a>
+
+<div id="ubicaciones">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Dirección</th>
+                <th>Provincia</th>
+                <th>Ciudad</th>
+                <th>Código postal</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $__currentLoopData = $locations; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $location): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <tr>
+                <td><?php echo e($location->id); ?></td>
+                <td><?php echo e($location->address); ?></td>
+                <td><?php echo e($location->province); ?></td>
+                <td><?php echo e($location->city); ?></td>
+                <td><?php echo e($location->cp); ?></td>
+                <td>
+                    <!-- Botón para eliminar -->
+                    <form action="<?php echo e(route('ubicaciones.destroy', $location->id)); ?>" method="POST">
+                        <?php echo csrf_field(); ?>
+                        <?php echo method_field('DELETE'); ?>
+                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+
+                </td>
+            </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </tbody>
+    </table>
+    <?php echo e($locations->links()); ?>
+
+</div>
+<?php $__env->stopSection(); ?>
+
+
+
+<?php echo $__env->make('layout.adminlte-layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\UniServerZ\www\pajaroneta\resources\views/admin/locations/index.blade.php ENDPATH**/ ?>

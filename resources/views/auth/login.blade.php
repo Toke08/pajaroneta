3d1531f0ff882ae67cc5334929cb0b5370a9067f
pauct04 <122865298+pauct04@users.noreply.github.com>
2024-02-13 02:43:36 +0100

    estilos con estilos bb

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,59 @@
 @extends('layout.masterpage')
 
+
+@section('estilos')
+<style>
+
+.btn-primary {
+    border-radius: 1.5em;
+    background-color: #E5A200;
+    border: none;
+    color: white;
+}
+.btn-primary:hover{
+    background-color: #CA8F00;
+}
+.form-control {
+    border-radius: 1.5em;
+    border:0.4px solid #000000;
+}
+.card-body{
+        background-image: url('{{asset('img/landing_page/Trucks.png')}}'); /* Reemplaza 'ruta-de-tu-imagen.jpg' con la ruta de tu imagen de fondo */
+        background-size: 300px 400px; /* Ajusta el tamaño de la imagen para cubrir todo el contenedor */
+        background-position: :right; /* Centra la imagen en el contenedor */
+        background-repeat: no-repeat; /* Evita que la imagen se repita en el contenedor */
+
+}
+.question{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.card-header{
+    border:none;
+    text-align: center;
+    font-size: 1.2em;
+    background-color: #ffff;
+}
+</style>
+
+@endsection
+
 @section('contenido')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center ">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card text-align-center">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row mb-3 ">
+                            <label for="email" class="col-md-4 col-form-label ">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 justify-content-center text-center">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -39,7 +78,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="col mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,19 +90,21 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <div class="col mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                    <a class="nav-link" href="{{ route('register') }}">@lang('Register')</a>
-
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link col" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+                                <div class="question">
+                                    <p>¿No tienes una cuenta?</p>
+                                    <a class="nav-link" href="{{ route('register') }}">@lang('Register')</a>
+                                </div>
                             </div>
                         </div>
                     </form>
--- storage/framework/views/e8738279ac7584413ba69fcbce689ec00bef76a7.php
+++ storage/framework/views/e8738279ac7584413ba69fcbce689ec00bef76a7.php
@@ -4,6 +4,7 @@
 .landing_page {
     /* display:flex;
     flex-direction:row;*/
+    margin-top: 10%;
     place-items:center;
     justify-content:center;
     display: grid;
@@ -11,17 +12,19 @@
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); /* Puedes ajustar el ancho según tus necesidades */
     grid-auto-rows: auto;
     grid-auto-flow: dense;
+
 }
 .part1 img{
-    width:45em;
-    position: relative;
+    position: absolute;
     margin-left: 20%
 }
 .papa1 {
-    transform:translate(0px, -200px);
+    transform:translate(-850px, -430px);
+    width:50em;
 }
 .papa2 {
-    transform:translate(0px, -10px);
+    transform:translate(-800px, -100px);
+    width:45em;
 }
 .part2{
     display:grid;
@@ -30,16 +33,19 @@
 .part2 p{
     font-size: 1.5em;
     align-items: center;
+    text-align: center;
 }
 .part2 img{
-    width:45em;
+    width:40em;
 }
 .part3 img{
-    width: 45em;
-    position: relative;
-    margin: 0%;
-    padding: 0%;
+    position: absolute;
 }
+.hot{
+    transform:translate(-200px, -250px);
+    width: 50em;
+}
+/* quienes somos */
 .who{
     display: flex;
     flex-direction:row;
@@ -48,6 +54,7 @@
     background-color: #730000;
     color: #ffffff;
     background-position: center;
+    margin-top: 20%;
 
 }
 .who_info{
@@ -59,7 +66,16 @@
     height: 25em;
 }
 /* estilo mapa */
+.tit{
+    display: flex;
+    flex-direction: row;
+    align-items:center;
+}
+.tit img{
+    width: 7%;
+    margin-left: 3%;
 
+}
 #mapilla iframe{
     width: 100%;
 }
@@ -170,11 +186,11 @@
             <img class="papa1" src="<?php echo e(asset('img/landing_page/papas_1.png')); ?>" alt="">
         </div>
         <div class="part2">
-            <img src="<?php echo e(asset('img/landing_page/logo_pajar.png')); ?>" alt="">
-            <p>Delicias para celiacos e intolerantes a través de toda España</p>
+            <img src="<?php echo e(asset('img/landing_page/pajaro-02.png')); ?>" alt="">
+            <p><?php echo app('translator')->get('Delicacies for celiac and intolerant people throughout Spain'); ?></p>
         </div>
         <div class="part3">
-            <img src="<?php echo e(asset('img/landing_page/perro_caliente.png')); ?>" alt="">
+            <img class="hot" src="<?php echo e(asset('img/landing_page/perro_caliente.png')); ?>" alt="">
         </div>
     </div>
 
@@ -187,9 +203,9 @@
         <img src="<?php echo e(asset('img/landing_page/burger_der.png')); ?>" alt="">
     </div>
     <div id="mapilla">
-        <div>
-            <h2>Encuentrános</h2>
-            <img src="" alt="">
+        <div class="tit">
+            <h2>Encuentrános...</h2>
+            <img src="<?php echo e(asset('img/landing_page/pajatruck.png')); ?>" alt="">
         </div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.5165004228597!2d-2.9419887246831182!3d43.26155407767463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e502842c84087%3A0x539b319a98f8cfbe!2sC.%20del%20Lic.%20Poza%2C%2031%2C%20Abando%2C%2048011%20Bilbao%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1707666740981!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
